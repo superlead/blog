2c546c448b3a0a3436ea780532925250a6ac518d
superlead <superlead@foxmail.com>
2017-12-01 23:09:41 +0800

    change booleanfield

--- blogweb/comment/models.py
+++ blogweb/comment/models.py
@@ -7,16 +7,12 @@ from article.models import Articles
 
 
 class Comment(models.Model):
-    STATUS_ITEMS = (
-        (1, u'显示'),
-        (2, u'隐藏'),
-    )
     article = models.ForeignKey(Articles, verbose_name=u'文章')
     author = models.CharField(max_length=10, verbose_name=u'昵称')
     email = models.EmailField(verbose_name=u'邮箱')
     content = models.TextField(verbose_name=u'评论', default=True, help_text=u'请文明留言,遵守相关法律法规!')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name=u'添加时间')
-    status = models.PositiveIntegerField(default=2, choices=STATUS_ITEMS, verbose_name=u'是否显示')
+    status = models.BooleanField(default=False, verbose_name=u'是否显示')
 
     class Meta:
         verbose_name_plural = verbose_name = u'评论管理'
--- blogweb/config/models.py
+++ blogweb/config/models.py
@@ -6,17 +6,12 @@ from django.contrib.auth.models import User
 
 
 class Link(models.Model):
-    STATUS_ITEMS = (
-        (1, u'显示'),
-        (2, u'隐藏'),
-    )
-
     name = models.CharField(max_length=50, verbose_name=u'链接名称')
     href = models.URLField(verbose_name=u'链接地址')
     index = models.PositiveIntegerField(default=99, verbose_name=u'权限')
     owner = models.ForeignKey(User, verbose_name=u'创建人')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name=u'创建时间')
-    is_display = models.CharField(default=1, choices=STATUS_ITEMS, verbose_name=u'是否显示')
+    is_display = models.BooleanField(default=True, verbose_name=u'是否显示')
 
     class Meta:
         verbose_name_plural = verbose_name = u'友情链接'
@@ -24,10 +19,6 @@ class Link(models.Model):
 
 
 class SideBar(models.Model):
-    STATUS_ITEMS = (
-        (1, u'显示'),
-        (2, u'下线'),
-    )
 
     SIDE_TYPE = (
         (1, u'HTML'),
@@ -41,6 +32,7 @@ class SideBar(models.Model):
     content = models.CharField(max_length=500, blank=True, verbose_name=u'内容', help_text=u'如果设置的不是HTML类型，可为空')
     owner = models.ForeignKey(User, verbose_name=u'创建者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name=u'创建时间')
+    is_display = models.BooleanField(default=True, verbose_name=u'是否显示')
 
     class Meta:
         verbose_name = verbose_name_plural = u'侧边栏'
