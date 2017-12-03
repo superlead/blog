120141e2d5ded78e1020e67ca51aa4f38ecbbf44
superlead <superlead@foxmail.com>
2017-12-03 15:53:19 +0800

    add save_model

--- blogweb/article/admin.py
+++ blogweb/article/admin.py
@@ -8,14 +8,20 @@ from .models import Articles, Category, Tag
 
 @admin.register(Articles)
 class ArticlesAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        super(ArticlesAdmin, self).save_model(request, obj, form, change)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        super(TagAdmin, self).save_model(request, obj, form, change)
