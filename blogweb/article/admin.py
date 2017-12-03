a4f68ef70fd77ae5ea5131508cf5f19c1cf32516
superlead <superlead@foxmail.com>
2017-12-03 12:13:39 +0800

    register AdminModel insert into Admin

--- blogweb/article/admin.py
+++ blogweb/article/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Articles, Category, Tag
+
+
+class ArticlesAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Articles, ArticlesAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
